f59f7510b10a65931440fe4e48954184c0c3a9c8
Alex Sahm <sahm.alexander@pwc.com>
2019-08-16 08:52:19 +0200

    added Problem 2: Even Fibonacci sum

--- test/gauss_test.exs
+++ test/gauss_test.exs
@@ -2,7 +2,10 @@ defmodule GAUSSTest do
   use ExUnit.Case
   doctest GAUSS
 
-  test "greets the world" do
-    #assert GAUSS.hello() == :world
+  @tag timeout: :infinity
+  test "sum of fibonacci even elements" do
+    assert GAUSS.fib(2) == 1
+    assert GAUSS.fib(5) == 5
+    assert GAUSS.fib(6) == 8
   end
 end
