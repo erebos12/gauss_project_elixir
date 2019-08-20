aa81bfa6702ceb59d67018cbefe7a3b8975b2846
Alex Sahm <sahm.alexander@pwc.com>
2019-08-20 18:56:51 +0200

    removed infinity timeout from test

--- test/gauss_test.exs
+++ test/gauss_test.exs
@@ -2,14 +2,12 @@ defmodule GAUSSTest do
   use ExUnit.Case
   doctest GAUSS
 
-  @tag timeout: :infinity
   test "sum of fibonacci even elements" do
     assert GAUSS.fib(2) == 1
     assert GAUSS.fib(5) == 5
     assert GAUSS.fib(6) == 8
   end
 
-  @tag timeout: :infinity
   test "prime factorization" do
     assert GAUSS.prime_factorization(3, []) == [3]
     assert GAUSS.prime_factorization(25, []) == [5, 5]
@@ -18,7 +16,6 @@ defmodule GAUSSTest do
     assert GAUSS.prime_factorization(13195, []) == [29, 13, 7, 5]
   end
 
-  @tag timeout: :infinity
   test "max prime factor" do
     assert GAUSS.max_prime_factor(13195) == 29
     assert GAUSS.max_prime_factor(30) == 5
@@ -29,7 +26,6 @@ defmodule GAUSSTest do
     assert GAUSS.max_prime_factor(1200) == 5
   end
 
-  @tag timeout: :infinity
   test "find smallest prime factor" do
     assert GAUSS.find_smallest_prime_factor(200) == 2
     assert GAUSS.find_smallest_prime_factor(15) == 3
